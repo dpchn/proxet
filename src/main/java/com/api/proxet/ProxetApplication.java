ac761736bbe88d7e9ffa90c4813a2363c8bcf123
dpchn <durgaprasadchncse@gmail.com>
2018-03-18 18:16:40 +0530

    initialize

---
+++ src/main/java/com/api/proxet/ProxetApplication.java
@@ -0,0 +1,12 @@
+package com.api.proxet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxetApplication.class, args);
+	}
+}
